857d5920fefc5b6671172af79556ae570ab479f1
Nurdaulet <134767660+Nurdauletqwe@users.noreply.github.com>
2024-04-03 22:37:56 +0600

    Add files via upload

---
+++ dz_9.py
@@ -0,0 +1,23 @@
+'''1'''
+# data = [1, 2, 3, 4, 5, 6, 7]
+# result = {num: num**3 for num in data if num % 2 != 0}
+# print(result)
+
+'''2'''
+# data = [1, 2, 3, 4, 4, 5, 6, 6, 6, 7, 7]
+# even_numbers = {num for num in data if num % 2 == 0}
+# print(even_numbers)
+
+'''3'''
+# new_list = [i * 10 for i in range(10)]
+# print(new_list)
+
+'''4'''
+# def num7(n):
+#     for i in range(n):
+#         if i % 7 == 0:
+#             yield i
+
+# n = int(input('write number: '))
+# for num in num7(n):
+#     print(num)
